92915bad518de09a7f25b847ee21902556696f3d
gilbert <s3665887@student.rmit.edu.au>
2022-10-17 15:15:18 +1100

    install rsa libaray

--- mcpi_sec.py
+++ mcpi_sec.py
@@ -1,7 +1,13 @@
 from mcpi.minecraft import Minecraft
+import rsa
 
 # Assignment 3 main file
 # Feel free to modify, and/or to add other modules/classes in this or other files
 
 mc = Minecraft.create()
-mc.postToChat("Hello world")
+publicKey, privateKey = rsa.newkeys(512)
+message = "Hello world"
+encMessage = rsa.encrypt(message.encode(),publicKey)
+print(encMessage)
+# mc.postToChat(message)
+decMessage = rsa.decrypt(encMessage, privateKey).decode()
